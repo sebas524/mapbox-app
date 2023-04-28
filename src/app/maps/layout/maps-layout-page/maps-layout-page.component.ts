d6827b3af420af4b0090a5b5aa8eb2c02def60eb
sebas524 <104307242+sebas524@users.noreply.github.com>
2023-04-28 17:13:50 +0200

    implement sidebar

---
+++ src/app/maps/layout/maps-layout-page/maps-layout-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-maps-layout-page',
+  templateUrl: './maps-layout-page.component.html',
+  styles: [
+  ]
+})
+export class MapsLayoutPageComponent {
+
+}
