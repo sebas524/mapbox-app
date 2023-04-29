d790732cc57ae00fa8888880310cafd1bc59a858
sebas524 <104307242+sebas524@users.noreply.github.com>
2023-04-29 15:02:10 +0200

    implement range functionality

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,11 +1,67 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Map } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
 @Component({
   selector: 'app-zoom-range-page',
   templateUrl: './zoom-range-page.component.html',
   styles: [
-  ]
+    `
+      #map {
+        width: 100vw;
+        height: 100vh;
+        background-color: gray;
+      }
+    `,
+  ],
 })
-export class ZoomRangePageComponent {
+export class ZoomRangePageComponent implements AfterViewInit {
+  @ViewChild('map') divMap?: ElementRef;
+  public zoom: number = 10;
+  public map?: Map;
 
+  ngAfterViewInit(): void {
+    // * afterViewInit means after view has been initialized, which means,once you have all html refrences loaded, then:
+
+    console.log(this.divMap);
+    if (!this.divMap) {
+      throw "html element wasn't found";
+    }
+
+    this.map = new Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: this.zoom, // starting zoom
+    });
+
+    this.mapListeners();
+  }
+
+  mapListeners() {
+    if (!this.map) {
+      throw "map hasn't been initialized";
+    }
+
+    this.map.on('zoom', (event) => {
+      console.log(event);
+      this.zoom = this.map!.getZoom();
+    });
+    this.map.on('zoomend', (event) => {
+      if (this.map!.getZoom() < 18) {
+        return;
+      }
+      this.map!.zoomTo(18);
+    });
+  }
+
+  zoomIn() {
+    this.map?.zoomIn();
+  }
+  zoomOut() {
+    this.map?.zoomOut();
+  }
+  zoomChanged(value: string) {
+    this.zoom = Number(value);
+    this.map?.zoomTo(this.zoom);
+  }
 }
